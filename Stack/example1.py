ac2588577c95664f26d8626e5b87f53ac1ebe806
StewieC <makokha.stewart21@students.dkut.ac.ke>
2025-02-26 10:32:13 +0300

    Add stack implementation example and documentation

---
+++ Stack/example1.py
@@ -0,0 +1,18 @@
+stack = []
+
+stack.append('k')
+stack.append('o')
+stack.append('s')
+stack.append('h')
+stack.append('y')
+
+print('Initial stack')
+print(stack)
+
+print('\nElements popped from stack:')  
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+
+print('\nStack after elements are popped:')
+print(stack)
