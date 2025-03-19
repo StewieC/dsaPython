23c387a3a09fd7e01cbffceb45614e7303c5c95a
StewieC <makokha.stewart21@students.dkut.ac.ke>
2025-03-19 10:49:31 +0300

    insert at any index

--- Linkedists/exapmle1.py
+++ Linkedists/exapmle1.py
@@ -16,4 +16,21 @@ class LinkedList:
         self.head = new_node
 
     # method to add at any index
+    # indexing starts from 0
+    def insertAtIndex(self, data, index):
+        if index == 0:
+            self.insertAtBegin(data)
+            return
+        
+        position = 0
+        current_node = self.head
+        while current_node is not None and position +1 != index:
+            position += 1
+            current_node = current_node.next
+        
+        if current_node is not None:
+            new_node = Node(data)
+            new_node.next = current_node.next
+        else:
+            print("Index not present in the linkedlist")
     
