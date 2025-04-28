dbaa898bd8b473395a8815e5fa18a024c8a26ea9
StewieC <makokha.stewart21@students.dkut.ac.ke>
2025-04-28 20:31:22 +0300

    Refactor match-case structure to include month-based conditions for day selection

--- Python Basic/match/match2.py
+++ Python Basic/match/match2.py
@@ -12,7 +12,24 @@ match day:
         print("Thursday")
     case 5:
         print("Friday")
+        
+        # pipe character | is used to match multiple values`
     case 6 | 7:
         print("Weekend")
     case _:
         print("Invalid day") # default case
+        
+    
+# if statements as guards
+
+month = 5
+day = 4
+
+match day:
+    
+    case 1 | 2 | 3 | 4 | 5 if month:
+        print("a weekend in april")
+        
+    case 1 | 2| 3 | 4 | 5  if month == 5:
+        print("Second week")
+   
