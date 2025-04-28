431248fbe40cbf3c4f314e53e44cf41a9f0e5d03
StewieC <makokha.stewart21@students.dkut.ac.ke>
2025-04-28 21:00:48 +0300

    Add initial implementations of elseInForLoops.py and range.py; enhance test1.py with additional examples

---
+++ Python Basic/for loops/elseInForLoops.py
@@ -0,0 +1,10 @@
+"""
+the else statement in a for loop specifies a block of code to be executed
+when the loop has ended
+
+"""
+
+for x in range(5):
+    print(x, '\n')
+else:
+    print("loop ended")
