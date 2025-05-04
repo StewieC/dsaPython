7f3e6beeeca5aad5c469c390a6c825c8d1f62cc0
StewieC <makokha.stewart21@students.dkut.ac.ke>
2025-05-04 23:32:52 +0300

    Add initial implementation of classes and creatingClass.py demonstrating user-defined templates for objects

---
+++ OOP concepts/creatingClass.py
@@ -0,0 +1,8 @@
+# we use keyword class to create a class
+
+class Dog:
+    sound = "bark"  # class variable
+    
+# to create an object:
+dog1 = Dog()  # dog1 is an instance of Dog class
+print(dog1.sound)  # accessing class variable using object
